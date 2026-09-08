ea57864163443a92bda93e0d64cd6e77140d55e0
Roteesaikai <Newtonkung1@gmail.com>
2026-09-09 05:09:15 +0700

    ADD & UPDATE:Button.tsx, HabitForm.tsx, HabitList.tsx, Header.tsx

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Button from "./Components/Button";
+import { HabitForm } from "./Components/HabitForm";
+import { HabitList } from "./Components/HabitList";
+import { Header } from "./Components/Header";
+
+export default function APP() {
+  return (
+    <>
+      <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
+        <Header />
+        <HabitForm />
+        <HabitList />
+      </div>
+    </>
+  );
+}
---
+++ src/Components/Button.tsx
@@ -0,0 +1,39 @@
+import type { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+
+type Variant = "primary" | "secondary" | "ghost-destructive";
+type ButtonProps = {
+  variant?: Variant;
+} & ComponentProps<"button">;
+
+export default function Button({
+  variant = "primary",
+  className,
+  ...props
+}: ButtonProps) {
+  return (
+    <>
+      <button
+        {...props}
+        className={twMerge(
+          "transition-colors rounded px-2 py-1 disabled:opacity-30 disabled:cursor-not-allowed",
+          getVariantStyles(variant),
+          className
+        )}
+      ></button>
+    </>
+  );
+}
+
+function getVariantStyles(variant: Variant) {
+  switch (variant) {
+    case "primary":
+      return "bg-blue-600 hover:bg-blue-400";
+    case "secondary":
+      return "bg-zinc-700 hover:bg-zinc-600 text-zinc-400";
+    case "ghost-destructive":
+      return "text-red-800 hover:bg-red-600 hover:text-red-200";
+    default:
+      throw new Error(`Invalid variant: ${variant satisfies never}`);
+  }
+}
